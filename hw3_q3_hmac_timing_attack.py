0eb2255f03be0b0f493a93602efb33312cd92fd2
Will Tappen <wmt45@cornell.edu>
2022-03-31 23:18:59 -0400

    implement q3 successfully

--- hw3_q3_hmac_timing_attack.py
+++ hw3_q3_hmac_timing_attack.py
@@ -29,8 +29,25 @@ def checkHMAC_oracle(msg: bytes) -> bool:
 
 
 def forge_tag() -> bytes:
-    # TODO: fill in your answer here
-    pass
+    
+    data = getData_oracle()
+
+    tag = bytearray(4)
+    for i in range(4):
+        max_time = 0
+        b = 0
+        for v in range(256):
+            tag[i] = v
+            start_t = time.time_ns()
+            checkHMAC_oracle(data + tag)
+            elapsed_t = time.time_ns() - start_t
+            if elapsed_t > max_time:
+                max_time = elapsed_t
+                b = v
+        tag[i] = b
+            
+    return data + tag
+
 
 
 if __name__ == "__main__":
